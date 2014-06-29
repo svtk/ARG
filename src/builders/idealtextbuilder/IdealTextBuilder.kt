f7903272f1b35a99da2019878f8e2d7a18b084d8
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-06-29 11:11:48 +0400

    Support 'emphasis' tag in parsing fb2

--- src/builders/idealtextbuilder/IdealTextBuilder.kt
+++ src/builders/idealtextbuilder/IdealTextBuilder.kt
@@ -15,13 +15,38 @@ fun getFormattedText(fb2: File): FormattedText? {
     var factory = SAXParserFactory.newInstance()
     var saxParser = factory?.newSAXParser()
     val paragraphTextToPosition = arrayListOf<Pair<String, Int>>()
+    val lastSentenceParts = arrayListOf<Pair<String, Int>>()
+
+    fun addLastSentence() {
+        if (lastSentenceParts.isEmpty()) return
+        val lastSentence = lastSentenceParts.map{ it.first }.joinToString("")
+        paragraphTextToPosition.add(lastSentence to lastSentenceParts.first!!.second)
+        lastSentenceParts.clear()
+    }
 
     var handler = object : DefaultHandler() {
         var isText = true
+        var theSameSentence = false
 
         override fun startElement(uri: String, localName: String, qName: String, attributes: Attributes) {
             if (qName.equalsIgnoreCase("p")) {
                 isText = true;
+            }
+            else if (qName.equalsIgnoreCase("emphasis")) {
+                isText = true
+                theSameSentence = true
+            }
+        }
+
+
+        override fun endElement(uri: String?, localName: String, qName: String) {
+            if (qName.equalsIgnoreCase("p")) {
+                theSameSentence = false
+                addLastSentence()
+            }
+            else if (qName.equalsIgnoreCase("emphasis")) {
+                isText = true
+                theSameSentence = true
             }
         }
 
@@ -32,13 +57,13 @@ fun getFormattedText(fb2: File): FormattedText? {
                     s += ch!![i]
                 }
                 if (s != "\n") {
-                    paragraphTextToPosition.add(s to start)
+                    lastSentenceParts.add(s to start)
                 }
                 isText = false;
             }
         }
     };
-
+    addLastSentence()
     saxParser?.parse(fb2, handler);
     return getFormattedText(paragraphTextToPosition)
 }
